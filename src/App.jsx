a37789c681142f22d260ddb8d32f022a71cd5fe5
leoshliak <levshlyak@gmail.com>
2025-03-01 13:40:51 +0200

    now with films
    I replaced test data with films data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './styles/App.css'
-import cards from './data';
 import Header from './components/Header';
+import { fetchRandomPosters} from './api';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
 
 function App() {
-  const initialCards = cards.map(card => ({ ...card }));
-  const [memoryCards, setMemoryCards] = useState(cards.map(card => ({ ...card }))) 
+  let initialCards = [];
+  const [memoryCards, setMemoryCards] = useState([]) 
   const [score, setScore] = useState(0)
   const [best, setBest] = useState(null)
+  const [over, setOver] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function load() {
+      try {
+       const  results = await fetchRandomPosters()
+       setMemoryCards(results)
+       initialCards = results.map(result => ({...result}))
+       setOver(false)
+       setLoading(true)
+      } catch(error) {
+       console.error('Error loading data:', error);
+      } finally {
+        setTimeout(() =>{
+           setLoading(false)
+        }, 600)    
+      }
+    }
+ 
+    load()
+   }, [over])
 
   function handleClick(i) {
       const updatedCards = [...memoryCards];
@@ -18,17 +42,32 @@ function App() {
 
       if(updatedCards[i].clicks === 2) {
           alert('You lost');
-          setMemoryCards(initialCards);
+          
           if(best == null || s > best){
             setBest(s - 1)
           } 
           setScore(0)
           s = 0;
+          setMemoryCards(initialCards);
+          setOver(true)
+  
           
       } else {
           const shuffled = [...updatedCards].sort(() => Math.random() - 0.5);
           setMemoryCards(shuffled)
       }
+
+      if(score == memoryCards.length - 1) {      
+        setMemoryCards(initialCards)
+        setBest(s)
+        setScore(0)
+        s = 0;
+        alert('You win') 
+      }
+  }
+
+  if(loading == true) {
+    return  <Spin size='large' fullscreen />
   }
 
   return (
@@ -38,8 +77,9 @@ function App() {
         <main>
           <div className='container'>
               {memoryCards.map((card, index) => (
-                  <div key={index} onClick={() => handleClick(index)} style={{cursor: 'pointer', border: '1px solid #000'}}>
-                      <p>{card.title}</p>
+                  <div className='card' key={index} onClick={() => handleClick(index)}>
+                    <img src={card.image} alt="" style={{width: "100%"}} />
+                    <div className="card-text"><p>{card.title}</p></div>  
                   </div>
               ))}
           </div>
--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
-// Get your free API key from TMDB: https://www.themoviedb.org/settings/api
 const TMDB_API_KEY =  "68212d6c081e1b9520fc8f85cc34565f"  //import.meta.env.VITE_TMDB_API;
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500'; // w500 is medium size
 
-async function fetchRandomPosters() {
+ export async function fetchRandomPosters() {
   try {
     // Fetch popular movies (random page)
     const randomPage = Math.floor(Math.random() * 100) + 1;
@@ -12,28 +11,23 @@ async function fetchRandomPosters() {
     
     const data = await response.json();
     const movies = data.results;
-    console.log(movies)
-
-    // Get first 8 movies with valid posters and duplicate for pairs
+    //console.log(movies)
 
       const moviePairs = movies
       .filter(movie => movie.poster_path && movie.original_title)
-      .slice(0, 8)
+      .slice(0, 16)
       .flatMap(movie => [
-        { image: `${BASE_IMAGE_URL}${movie.poster_path}`, title: movie.title, clicks: 0 },
         { image: `${BASE_IMAGE_URL}${movie.poster_path}`, title: movie.title, clicks: 0 }
       ]);
 
-    // Shuffle the posters array
-    return shuffleArray(moviePairs);
+    return moviePairs;
   } catch (error) {
     console.error( error);
     return [];
   }
 }
 
-// Fisher-Yates shuffle algorithm
-function shuffleArray(array) {
+export function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -41,10 +35,7 @@ function shuffleArray(array) {
   return array;
 }
 
-// Usage example
-fetchRandomPosters().then(moviePairs => {
+/*fetchRandomPosters().then(moviePairs => {
   console.log('Shuffled poster URLs:', moviePairs   );
-  // Use these URLs to create your game cards
-});
-
+});*/
 
